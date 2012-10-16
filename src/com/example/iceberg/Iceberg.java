b9e6221eebfba4f437084693cbcc40e7d771e0d6
N Harvey <nthnhrvy@gmail.com>
2012-10-16 16:46:14 -0600

    Should now do events

--- src/com/example/iceberg/Iceberg.java
+++ src/com/example/iceberg/Iceberg.java
@@ -2,29 +2,38 @@ package com.example.iceberg;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
 import android.view.MotionEvent;
+import android.view.View;
 import android.widget.Toast;
 
 public class Iceberg extends Activity {
 	private int icebergSize;
-	
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_iceberg);
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_iceberg, menu);
-        return true;
-    }
-    
-    public boolean onClick(MotionEvent e) {
-    	if(Settings.getIcebergs()) icebergSize=20;
-    	Toast.makeText(this, "you clicked", Toast.LENGTH_SHORT).show();
-    	//for()
-    	return true;
-    }
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_iceberg);
+	}
+	
+	@Override
+	public boolean onTouchEvent(MotionEvent e) {
+		if(Settings.getIcebergs()) icebergSize=Settings.getSize();
+		String x = String.valueOf(e.getX());
+		Toast.makeText(this, "you clicked at "+x, Toast.LENGTH_SHORT).show();
+		//for()
+		return true;
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_iceberg, menu);
+		return true;
+	}
+	
+	public void pullUpSettings(View view){
+		Intent intent = new Intent(this, Settings.class);
+		startActivity(intent);
+	}
 }
--- src/com/example/iceberg/Settings.java
+++ src/com/example/iceberg/Settings.java
@@ -2,25 +2,35 @@ package com.example.iceberg;
 
 import android.os.Bundle;
 import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ToggleButton;
 import android.app.Activity;
 import android.content.res.Resources;
 
 public class Settings extends Activity {
 	private static boolean icebergs=true;
+	private static int size;
 	
-	public boolean switcheroo(MotionEvent e){
+	public void switcheroo(View view){
 		icebergs = !icebergs;
-		return true;
+		ToggleButton onoff = (ToggleButton) view;
+		onoff.setChecked(icebergs);
+		onoff.setText(icebergs?"On":"Off");
 	}
 	
 	public static boolean getIcebergs(){
 		return icebergs;
 	}
 	
+	public static int getSize(){
+		return size;
+	}
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        size=25;
     }
 
 }
